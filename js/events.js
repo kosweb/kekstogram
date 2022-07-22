dccc5d51f9eb43d34f3abc5b2c982d6273c18759
kosweb <kosweblink@gmail.com>
2022-07-22 12:33:16 +0300

    add events on all filters

--- js/events.js
+++ js/events.js
@@ -43,6 +43,7 @@ effectItems.forEach((el, i) => {
 		if (i === 0) {
 			rangeField.style = 'display: none';
 			imgUploadPreview.removeAttribute('class');
+			imgUploadPreview.removeAttribute('style');
 		} else {
 			rangeField.style = 'display: flex';
 			rangeInput.value = rangeMaxValue;
@@ -54,37 +55,62 @@ effectItems.forEach((el, i) => {
 		switch (i) {
 			case 1:
 				imgUploadPreview.removeAttribute('class');
-				imgUploadPreview.classList.add('effects__preview--chrome')
+				imgUploadPreview.removeAttribute('style');
+				imgUploadPreview.classList.add('effects__preview--chrome');
 				break;
 			case 2:
 				imgUploadPreview.removeAttribute('class');
-				imgUploadPreview.classList.add('effects__preview--sepia')
+				imgUploadPreview.removeAttribute('style');
+				imgUploadPreview.classList.add('effects__preview--sepia');
 				break;
 			case 3:
 				imgUploadPreview.removeAttribute('class');
-				imgUploadPreview.classList.add('effects__preview--marvin')
+				imgUploadPreview.removeAttribute('style');
+				imgUploadPreview.classList.add('effects__preview--marvin');
 				break;
 			case 4:
 				imgUploadPreview.removeAttribute('class');
-				imgUploadPreview.classList.add('effects__preview--phobos')
+				imgUploadPreview.removeAttribute('style');
+				imgUploadPreview.classList.add('effects__preview--phobos');
 				break;
 			case 5:
 				imgUploadPreview.removeAttribute('class');
-				imgUploadPreview.classList.add('effects__preview--heat')
+				imgUploadPreview.removeAttribute('style');
+				imgUploadPreview.classList.add('effects__preview--heat');
 				break;
 		}
 	});
 });
-
-const rangeInputFunc = function(filterName) {
-
-};
 
 rangeInput.addEventListener('input', function() {
 	rangeInputValue.textContent = this.value;
 	let x = this.value;
 	let color = `linear-gradient(90deg, rgba(255, 231, 83, 1) ${x}%, rgba(255, 231, 83, 0.5) ${x}%)`;
 	this.style.background = color;
+
+	if (x === 100) {
+		x = 1;
+	} else {
+		x /= 100;
+	}
+
+	switch (imgUploadPreview.className) {
+		case 'effects__preview--chrome':
+			imgUploadPreview.style = `filter: grayscale(${x})`;
+			break;
+		case 'effects__preview--sepia':
+			imgUploadPreview.style = `filter: sepia(${x})`;
+			break;
+		case 'effects__preview--marvin':
+			imgUploadPreview.style = `filter: invert(${x * 100}%)`;
+			break;
+		case 'effects__preview--phobos':
+			imgUploadPreview.style = `filter: blur(${x * 3}px)`;
+			break;
+		case 'effects__preview--heat':
+			imgUploadPreview.style = `filter: brightness(${1 + x * 2})`;
+			break;
+	}
 });
 
 const onImgUploadOverlayEscPress = (evt) => {
